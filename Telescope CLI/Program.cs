2e9dabaad95ad31b5ee3da7e6551c63cbad8de5d
Schlafhase <schlafhase190@gmail.com>
2025-03-14 19:13:52 +0100

    Add pagination.

--- Telescope CLI/Program.cs
+++ Telescope CLI/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace Telescope_CLI;
+﻿using Microsoft.Azure.Cosmos;
+using Telescope;
 
-class Program
+namespace Telescope_CLI;
+
+internal abstract class Program
 {
-	static void Main(string[] args)
+	private static readonly string _connectionString;
+	
+	private static async Task Main(string[] args)
 	{
-		Console.WriteLine("Hello, World!");
+		CosmosClient client = new CosmosClient(_connectionString);
+		CosmosApiWrapper cosmosApiWrapper = new(client);
+		
+		cosmosApiWrapper.SelectDatabase("cactus-messenger");
+		cosmosApiWrapper.SelectContainer("cactus-messenger");
+		
+		await cosmosApiWrapper.GetFirstPageByQueryAsync("""
+														SELECT * FROM c WHERE c.Type = "message" ORDER BY c.Content
+														""");
+		
+		foreach (dynamic entity in cosmosApiWrapper._loadedEntities)
+		{
+			Console.WriteLine(entity);
+		}
+		
+		Console.WriteLine("Done");
+
+		await cosmosApiWrapper.LoadMore();
+		
+		foreach (dynamic entity in cosmosApiWrapper._loadedEntities)
+		{
+			Console.WriteLine(entity);
+		}
 	}
 }
